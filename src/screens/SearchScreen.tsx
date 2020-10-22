83149e0dcaa1a04a9ea6dbe3528a3479eef48506
Abdullah Alshaye <34545177+Abdullah700@users.noreply.github.com>
2020-10-22 14:33:38 +0300

    ..

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -2,9 +2,21 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
+import { IBusiness } from "../interfaces/IBusiness";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState<string>("");
+  const [results, setResults] = useState<IBusiness[]>([]);
+
+  const searchApi = async (): Promise<void> => {
+    const response = await yelp.get("/search", {
+      params: {
+        limit: 50,
+      },
+    });
+    setResults(response.data.businesses);
+  };
   return (
     <View>
       <SearchBar
@@ -12,7 +24,7 @@ const SearchScreen = () => {
         onTermChange={(t: string) => setTerm(t)}
         onTermSubmit={() => console.log("submitted")}
       />
-      <Text>{term}</Text>
+      <Text>{results.length} has been found</Text>
     </View>
   );
 };
